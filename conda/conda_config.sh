59c84fcaf5ee35eccc2a8f34125fb1deaf428cdd
zyzisyz <zyziszy@foxmail.com>
2018-10-09 15:43:25 +0800

    update

---
+++ conda/conda_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Anaconda https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/
+# Miniconda https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/
+
+# thu
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
