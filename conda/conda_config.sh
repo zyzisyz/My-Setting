362910f81e97faf283064e8e9a7ce51a0f7f6af1
zyz <zyziszy@foxmail.com>
2018-10-13 15:44:09 +0800

    update conda command

--- conda/conda_config.sh
+++ conda/conda_config.sh
@@ -7,3 +7,29 @@
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
 conda config --set show_channel_urls yes
+
+####################################################################################
+
+# conda command
+
+# env create: 
+# conda create -n env_name python=3.6
+
+# env remove
+# conda remove -n env_name --all
+
+# env activate: 
+# source activate env_name
+
+# env deactivate
+# source deactivate env_name
+
+# ls env: 
+# conda env list
+
+# version: 
+# conda -V
+
+# env share (in target_env)
+# conda env export > environment.yml
+# conda env create -f environment.yml
